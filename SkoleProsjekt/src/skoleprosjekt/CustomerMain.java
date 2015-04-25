59c25788ad3950d48f9691b17b38550ec5536fd5
Sondre <Sondre@158.38.208.197>
2015-04-25 16:27:41 +0200

    Designendringer

--- SkoleProsjekt/src/skoleprosjekt/CustomerMain.java
+++ SkoleProsjekt/src/skoleprosjekt/CustomerMain.java
@@ -124,6 +124,7 @@ public class CustomerMain extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void centerSelected(javax.swing.event.ListSelectionEvent evt) {//GEN-FIRST:event_centerSelected
